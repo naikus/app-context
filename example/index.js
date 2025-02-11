729f56b5f8cd0f8fc35375954aa9dc23b01e5f74
Vesloo <wesley.pigny@gmail.com>
2025-02-11 20:05:09 +0100

    Loading the dependencies dynamically

--- example/index.js
+++ example/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const createAppContext = require("../lib/context"),
     app = createAppContext();
 
@@ -13,74 +16,71 @@ app.on(
 );
 
 
+// Read the package.json file
+const packageJsonPath = path.resolve(__dirname, '../package.json');
+const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+
+// Parse the JSON content
+const packageJson = JSON.parse(packageJsonContent);
+
+// Access the dependencies
+const dependencies = packageJson.dependencies;
+const devDependencies = packageJson.devDependencies;
+
+// Log the dependencies
+// console.log(devDependencies);
+
+// Register modules dynamically based on dependencies
+/**
+ * @param {Object} dependencies
+ */
+
+const registerModules = async (dependencies) => {
+  if (!dependencies)
+    return;
+  const modulePromises = Object.keys(dependencies).map(dep => {
+    return app.register({
+      name: dep,
+      initialize(ctx) {
+        return {
+          toString() {
+            return dep;
+          }
+        };
+      }
+    });
+  });
+
+  await Promise.all(modulePromises);
+};
+
+// Register modules dynamically based on devDependencies
+/**
+ * @param {Object} devDependencies
+ */
+const registerDevModules = async (devDependencies) => {
+  const modulePromises = Object.keys(devDependencies).map(dep => {
+    return app.register({
+      name: dep,
+      initialize(ctx) {
+        return {
+          toString() {
+            return dep;
+          }
+        };
+      }
+    });
+  });
+
+  await Promise.all(modulePromises);
+};
+
+// Register both dependencies and devDependencies
 Promise.all([
-  app.register({
-    name: "module_a",
-    async initialize(ctx) {
-      const [modB, modC, modDFac] = await ctx.dependency(["module_b", "module_c", "module_d_factory"]);
-      return {
-        get name() {
-          return `MODULE_A <-> ${modDFac().name}`;
-        },
-        toString() {
-          return `modA ${modB} ${modC}`;
-        }
-      };
-    }
-  }),
-  app.register({
-    name: "module_b",
-    initialize(ctx) {
-      return new Promise((res, reject) => {
-        ctx.dependency("module_c", modC => {
-          console.log(`[module_b] Loading...`);
-          setTimeout(_ => {
-            console.log(`[module_b] Done!`);
-            res({
-              toString() {
-                return `[modB ${modC}]`;
-              }
-            });
-          }, 1000);
-        });
-      });
-    }
-  }),
-  app.register({
-    name: "module_c",
-    initialize(ctx) {
-      return {
-        toString() {
-          return "modC";
-        }
-      };
-    }
-  }),
-  // Workaround for circular deps, create a factory
-  app.register({
-    name: "module_d_factory",
-    initialize(ctx) {
-      /** @type {Object|null|undefined} */
-      let module;
-      return () => {
-        if(!module) {
-          const modA = ctx.getModule("module_a");
-          module = {
-            name: "MODULE_D",
-            toString() {
-              return `[modD circular dep: ${modA.name}]`;
-            }
-          };
-        }
-        return module;
-      };
-    }
-  })
+  registerModules(dependencies),
+  registerDevModules(devDependencies),
 ]).then(async () => {
   console.log("Ready!");
-  const modFac = app.getModule("module_d_factory"),
-      mod = await modFac();
-  console.log(mod.toString());
 });
 
 
